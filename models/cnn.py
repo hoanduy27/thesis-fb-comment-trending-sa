ad8496600788a11f130eedb6e1ef017fce9d2b1e
hoanduy27 <50733059+hoanduy27@users.noreply.github.com>
2021-10-15 02:07:33 +0700

    Delete model.compile from models.cnn.SALT

--- models/cnn.py
+++ models/cnn.py
@@ -4,9 +4,10 @@ from tensorflow.keras.layers import Embedding, Flatten, Dense, Input, Softmax
 from tensorflow.keras.layers import Conv1D, MaxPool1D, Dropout, BatchNormalization
 from tensorflow.keras.layers import concatenate
 from tensorflow.keras.regularizers import l1_l2
+from tensorflow.keras.optimizers import Adam
  
- 
-def SALT(input_length, input_dim, embedding_dim, output_dim, num_kernels, kernel_size, pool_size, embedding_dropout, conv_dropout, loss=None, embedding_matrix=None):
+
+def SALT(input_length, input_dim, embedding_dim, output_dim, num_kernels, kernel_size, pool_size, embedding_dropout, conv_dropout, embedding_matrix=None):
     """SALT model, proposed in https://arxiv.org/abs/1806.08760"""
     inp = Input(shape=(input_length,))
     embedding = Embedding(input_dim=input_dim, output_dim=embedding_dim, input_length=input_length, mask_zero=False, weights=[embedding_matrix] if embedding_matrix is not None else None)(inp)
@@ -32,18 +33,5 @@ def SALT(input_length, input_dim, embedding_dim, output_dim, num_kernels, kernel
 
     model = Model(inputs = inp, outputs = op)
 
-    if loss is not None:
-        loss = loss
-    else:
-      if output_dim==1:
-        loss = 'binary_crossentropy'
-      else:
-        loss = 'categorical_crossentropy'
-        
-    model.compile(
-        loss = loss, 
-        optimizer = Adam(learning_rate=.001, beta_1=.9, beta_2=.99, epsilon=1e-8),
-        metrics = [tf.keras.metrics.AUC(),
-                   'accuracy']
-    )
+    
     return model
