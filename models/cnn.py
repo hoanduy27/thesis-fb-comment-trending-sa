25f8d89d46e636f4a218be550634c8e75f524d54
root <hoanduy27@gmail.com>
2021-10-15 10:35:20 +0000

    Add BASLT, SALTA (testing failed)

--- models/cnn.py
+++ models/cnn.py
@@ -1,16 +1,28 @@
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Embedding, Flatten, Dense, Input, Softmax
-from tensorflow.keras.layers import Conv1D, MaxPool1D, Dropout, BatchNormalization
+from tensorflow.keras.layers import Conv1D, MaxPool1D, Dropout, BatchNormalization, LayerNormalization, MultiHeadAttention, Add
 from tensorflow.keras.layers import concatenate
 from tensorflow.keras.regularizers import l1_l2
 from tensorflow.keras.optimizers import Adam
  
+def MHSA(num_heads, key_dim, value_dim):
+  from tensorflow.keras.layers import LayerNormalization, MultiHeadAttention, Dense, Add
+  def calc(input, attention_mask):
+    mhsa = LayerNormalization()(input)
+    mhsa = MultiHeadAttention(num_heads=num_heads, key_dim=key_dim, value_dim=value_dim, dropout=0.2)
+    mhsa = mhsa(input, input, attention_mask=attention_mask)
+
+    add1 = Add()([input, mhsa])
+    add1 = LayerNormalization()(add1)
+
+    return add1
+  return calc
 
 def SALT(input_length, input_dim, embedding_dim, output_dim, num_kernels, kernel_size, pool_size, embedding_dropout, conv_dropout, embedding_matrix=None):
     """SALT model, proposed in https://arxiv.org/abs/1806.08760"""
     inp = Input(shape=(input_length,))
-    embedding = Embedding(input_dim=input_dim, output_dim=embedding_dim, input_length=input_length, mask_zero=False, weights=[embedding_matrix] if embedding_matrix is not None else None)(inp)
+    embedding = Embedding(input_dim=input_dim, output_dim=embedding_dim, input_length=input_length, mask_zero=False, weights=[embedding_matrix] if embedding_matrix is not None else None, name='embedding')(inp)
     embedding = Dropout(embedding_dropout)(embedding)
 
     conv = []
@@ -32,6 +44,77 @@ def SALT(input_length, input_dim, embedding_dim, output_dim, num_kernels, kernel
     op = Softmax()(dense)
 
     model = Model(inputs = inp, outputs = op)
+
+    
+    return model
+
+
+def SALTA(input_length, input_dim, embedding_dim, output_dim, attention_heads, key_dim, value_dim, num_kernels, kernel_size, pool_size, embedding_dropout, conv_dropout, embedding_matrix=None, embedding_trainable=False):
+    inp = Input(shape=(input_length,), name='input')
+    inp_masked = Input(shape=(input_length,), name='input_masked')
+
+    embedding = Embedding(input_dim=input_dim, output_dim=embedding_dim, input_length=input_length, mask_zero=False, weights=[embedding_matrix] if embedding_matrix is not None else None, trainable=embedding_trainable, name='embedding')(inp)
+    embedding = Dropout(embedding_dropout)(embedding)
+
+    mhsa = MHSA(attention_heads, key_dim, value_dim)(embedding, attention_mask=inp_masked)
+
+    conv = []
+    for i in range(num_kernels):
+      conv += [Conv1D(1, kernel_size=kernel_size, padding='same', activation='relu', kernel_regularizer=l1_l2(0.01, 0.01))(mhsa)]
+    
+    concat = concatenate(conv)
+    concat = BatchNormalization()(concat)
+
+    conv1 = Conv1D(1, kernel_size=kernel_size, padding='same', activation='relu', kernel_regularizer=l1_l2(0.01, 0.01))(concat)
+    conv1 = BatchNormalization()(conv1)
+
+    add = Add()([concat, conv1])
+
+    pool = MaxPool1D(pool_size=pool_size)(add)
+    pool = BatchNormalization()(pool)
+
+    flatten = Flatten()(pool)
+    flatten = Dropout(rate=conv_dropout)(flatten)
+    
+    dense = Dense(64, activation='relu', kernel_regularizer=l1_l2(0.01, 0.01))(flatten)
+    dense = Dense(output_dim, kernel_regularizer=l1_l2(0.01, 0.01))(dense)
+
+    op = Softmax()(dense)
+
+    model = Model(inputs = [inp, inp_masked], outputs = op)
+
+    return model
+
+
+
+def BSALT(input_length, input_dim, output_dim, num_kernels, kernel_size, pool_size, embedding_dropout, conv_dropout, bert_embedding):
+    """Proposed: BSALT model"""
+    inp = Input(shape=(input_length,), dtype='int32')
+    inp_masked = Input(shape=(input_length,), dtype='int32')
+
+    embedding = bert_embedding(inp, attention_mask=inp_masked)[0]
+    embedding = Dropout(embedding_dropout)(embedding)
+
+    conv = []
+    for i in range(num_kernels):
+      conv += [Conv1D(1, kernel_size=kernel_size, padding='same', activation='relu', kernel_regularizer=l1_l2(0.01, 0.01))(embedding)]
+    
+    concat = concatenate(conv)
+    concat = BatchNormalization()(concat)
+
+    pool = MaxPool1D(pool_size=pool_size)(concat)
+    pool = BatchNormalization()(pool)
+
+    flatten = Flatten()(pool)
+    flatten = Dropout(rate=conv_dropout)(flatten)
+    
+    dense = Dense(64, activation='relu', kernel_regularizer=l1_l2(0.01, 0.01))(flatten)
+    dense = Dense(output_dim, kernel_regularizer=l1_l2(0.01, 0.01))(dense)
+
+    op = Softmax()(dense)
+
+    model = Model(inputs = [inp, inp_masked], outputs = op)
+    model.layers[2].trainable = False
 
     
     return model
--- train.py
+++ train.py
@@ -4,8 +4,9 @@ import tensorflow as tf
 from sklearn.model_selection import StratifiedKFold
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, TensorBoard, CSVLogger
 from tensorflow.keras.utils import to_categorical
-from models.cnn import SALT
+from models.cnn import SALT, BSALT, SALTA
 from tensorflow.keras.optimizers import Adam, SGD, Adadelta
+
 cur_dir = os.path.abspath(__file__)
 cur_dir = os.path.dirname(cur_dir)
 
@@ -110,6 +111,160 @@ def train_SALT(name, X, y, n_folds=1, batch_size=8, epochs=10, **kwargs):
       batch_size=batch_size,
       epochs=epochs, 
       validation_data=(X[val_idx], y_cat[val_idx]), 
+      callbacks = callbacks
+    )
+    print()
+
+
+def train_BSALT(name, X, X_masked, y, n_folds=1, batch_size=8, epochs=10, **kwargs):
+  model_dir = f'{cur_dir}/logs/bsalt/{name}'
+  kfolds = StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=7).split(X, y)
+  y_cat = to_categorical(y)
+  
+  optim = kwargs.get('optim', 'adam')
+  optim_conf = kwargs.get('optim_conf', {})
+
+  for i, (train_idx, val_idx) in enumerate(kfolds):
+    print(f'Fold {i}:')
+    print('>'*60)
+
+    bsalt_cv = BSALT(
+      input_length = kwargs.get('input_length'),
+      input_dim = kwargs.get('input_dim'),
+      output_dim = kwargs.get('output_dim', y_cat.shape[1]),
+      num_kernels = kwargs.get('num_kernels', 3),
+      kernel_size = kwargs.get('kernel_size', 3),
+      pool_size = kwargs.get('pool_size', 2),
+      embedding_dropout = kwargs.get('embedding_dropout', 0.2),
+      conv_dropout = kwargs.get('conv_dropout', 0.2),
+      bert_embedding = kwargs.get('bert_embedding')
+    )
+    # Save model config
+    if i==0:
+      if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+
+      tf.keras.utils.plot_model(bsalt_cv, to_file=f'{model_dir}/model.png', show_shapes=True, show_dtype=True)
+
+      from contextlib import redirect_stdout
+      with open(f'{model_dir}/summary.txt', 'w') as f:
+        with redirect_stdout(f):
+          bsalt_cv.summary()
+
+      
+    # Callbacks
+    callbacks = create_callbacks(
+      model_path = f'{model_dir}/fold_{i}/model',
+      tensorboard_path = f'{model_dir}/fold_{i}/tensorboard', 
+      log_path = f'{model_dir}/fold_{i}/history.csv',
+      factor = kwargs.get('factor', .1),
+      cooldown= kwargs.get('cooldown', 2),
+      patience = kwargs.get('patience', 1)
+    )
+    
+    # Loss
+    loss = kwargs.get('loss', None)
+    if loss is None:
+      if output_dim==1:
+        loss = 'binary_crossentropy'
+      else:
+        loss = 'categorical_crossentropy'
+        
+    # Compile model
+    optimizer = get_optimizer(optim, optim_conf)
+    bsalt_cv.compile(
+        loss = loss, 
+        optimizer = optimizer,
+        metrics = [tf.keras.metrics.AUC(),
+                   'accuracy']
+    )
+
+    # Start training
+    bsalt_cv.fit(
+      [X[train_idx], X_masked[train_idx]],
+      y_cat[train_idx], 
+      batch_size=batch_size,
+      epochs=epochs, 
+      validation_data=([X[val_idx], X_masked[val_idx]], y_cat[val_idx]), 
+      callbacks = callbacks
+    )
+    print()
+
+def train_SALTA(name, X, X_masked, y, n_folds=1, batch_size=8, epochs=10, **kwargs):
+  model_dir = f'{cur_dir}/logs/salta/{name}'
+  kfolds = StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=7).split(X, y)
+  y_cat = to_categorical(y)
+  
+  optim = kwargs.get('optim', 'adam')
+  optim_conf = kwargs.get('optim_conf', {})
+
+  for i, (train_idx, val_idx) in enumerate(kfolds):
+    print(f'Fold {i}:')
+    print('>'*60)
+
+    salta_cv = SALTA(
+      input_length = kwargs.get('input_length'),
+      input_dim = kwargs.get('input_dim'),
+      embedding_dim = kwargs.get('embedding_dim'),
+      output_dim = kwargs.get('output_dim', y_cat.shape[1]),
+      attention_heads = kwargs.get('attention_heads', 4), 
+      key_dim = kwargs.get('key_dim', 256), 
+      value_dim = kwargs.get('value_dim', 256),
+      num_kernels = kwargs.get('num_kernels', 3),
+      kernel_size = kwargs.get('kernel_size', 3),
+      pool_size = kwargs.get('pool_size', 2),
+      embedding_dropout = kwargs.get('embedding_dropout', 0.2),
+      conv_dropout = kwargs.get('conv_dropout', 0.2),
+      embedding_matrix = kwargs.get('embedding_matrix', None),
+      embedding_trainable = kwargs.get('embedding_trainable', False)
+    )
+    # Save model config
+    if i==0:
+      if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+
+      tf.keras.utils.plot_model(salta_cv, to_file=f'{model_dir}/model.png', show_shapes=True, show_dtype=True)
+
+      from contextlib import redirect_stdout
+      with open(f'{model_dir}/summary.txt', 'w') as f:
+        with redirect_stdout(f):
+          salta_cv.summary()
+
+      
+    # Callbacks
+    callbacks = create_callbacks(
+      model_path = f'{model_dir}/fold_{i}/model',
+      tensorboard_path = f'{model_dir}/fold_{i}/tensorboard', 
+      log_path = f'{model_dir}/fold_{i}/history.csv',
+      factor = kwargs.get('factor', .1),
+      cooldown= kwargs.get('cooldown', 2),
+      patience = kwargs.get('patience', 1)
+    )
+    
+    # Loss
+    loss = kwargs.get('loss', None)
+    if loss is None:
+      if output_dim==1:
+        loss = 'binary_crossentropy'
+      else:
+        loss = 'categorical_crossentropy'
+        
+    # Compile model
+    optimizer = get_optimizer(optim, optim_conf)
+    salta_cv.compile(
+        loss = loss, 
+        optimizer = optimizer,
+        metrics = [tf.keras.metrics.AUC(),
+                   'accuracy']
+    )
+
+    # Start training
+    salta_cv.fit(
+      [X[train_idx], X_masked[train_idx]],
+      y_cat[train_idx], 
+      batch_size=batch_size,
+      epochs=epochs, 
+      validation_data=([X[val_idx], X_masked[val_idx]], y_cat[val_idx]), 
       callbacks = callbacks
     )
     print()
